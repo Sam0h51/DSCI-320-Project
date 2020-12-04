6f7d455e95b74ad4ccfb0ca036c1d96e47e67ca1
SamS <sscheuerman@student.cccs.edu>
2020-12-03 18:10:55 -0700

    Update SimpleNueralNet.py

--- SimpleNueralNet.py
+++ SimpleNueralNet.py
@@ -8,6 +8,9 @@ import numpy as np
 N = 100
 sigma = 0.3
 
+## Setting up the data plots
+fig, axs = plt.subplots(1, 2)
+
 ## First, we want the red data
 w = np.array(())
 y = np.array(())
@@ -25,7 +28,7 @@ print(w.shape)
 
 ## Plotting the red data
 for i in range(w[0].size):
-    plt.plot(w[0][i], w[1][i], 'r.')
+    axs[0].plot(w[0][i], w[1][i], 'r.')
 
 ## Now, we need the blue data
 u = np.array(())
@@ -50,7 +53,7 @@ print(u.shape)
 
 ## Plotting blue data
 for i in range(u[0].size):
-    plt.plot(u[0][i], u[1][i], 'b.')
+    axs[0].plot(u[0][i], u[1][i], 'b.')
 
 
 y = y.reshape(600, 2)
@@ -64,20 +67,24 @@ def get_w(x):
     w2 = x[10:35]
     w3 = x[35:45]
 
-    w1 = w1.shape(5, 2)
-    w2 = w2.shape(5, 5)
-    w3 = w3.shape(2, 5)
+    w1 = w1.reshape(5, 2)
+    w2 = w2.reshape(5, 5)
+    w3 = w3.reshape(2, 5)
 
     return w1, w2, w3
 
 ## This function parses the back of the 1d input array into the 3
 ## bias vectors
 def get_b(x):
-    b1 = x[45, 50]
-    b2 = x[50, 55]
-    b3 = x[55, 57]
+    b1 = x[45:50]
+    b2 = x[50:55]
+    b3 = x[55:57]
 
-    return b1.T, b2.T, b3.T
+    b1 = b1.reshape(5, 1)
+    b2 = b2.reshape(5, 1)
+    b3 = b3.reshape(2, 1)
+
+    return b1, b2, b3
 
 ## This function applies the sigma function to all elements of an
 ## input vector
@@ -90,13 +97,15 @@ def sigma_f(x):
 ## and a 2 node output layer
 def f(x, w1, w2, w3, b1, b2, b3):
     ## Layer 1          (2x1 --> 5x1)
-    xk = sigma_f(w1.dot(x) + b1)
+    xk = np.matmul(w1, x) + b1
+    xk = sigma_f(xk)
 
     ## Layer 2          (5x1 --> 5x1)
-    xk = sigma_f(w2.dot(xk) + b2)
+    xk = np.matmul(w2, xk) + b2
+    xk = sigma_f(xk)
 
     ## Layer 3, output  (5x1 --> 2x1)
-    xk = sigma_f(w3.dot(xk) + b3)
+    xk = sigma_f(np.matmul(w3, xk) + b3)
 
     return xk
     
@@ -123,9 +132,83 @@ def obj_f(x, blue, red, y):
         
     return tot/600
 
-## Now, I'm going to try to use simulated annealing to minimize
+## Now, I'm going to try to use very fast simulated annealing to minimize
 ## the objective function
 
+## VFSA Setup
+
+T = 20
+sig = 20
+N = 100
+e = 0.01
+k = 0
+
+x0 = np.ones(57)
+lowest = obj_f(x0, w, u, y)
+lowx = x0
+
+uk = np.array(k)
+vk = np.array(lowest)
+wk = np.array(lowest)
+
+## A function to get the next input
+def get_next(x, sig):
+    for i in range(x.size):
+        x[i] = x[i] + rn.uniform(-1, 1)*sig
+    return x
+
+## VFSA Iterations
+for i in range(N):
+    print('LET THERE BE NO DOUBT! K IS ', k)
+    sigk = sig/(1 + e*k)
+    Tk = T/(1 + e*k)
+    xt = get_next(x0, sigk)
+    
+    y1 = obj_f(x0, w, u, y)
+    y2 = obj_f(xt, w, u, y)
+
+    if(y2 < y1):
+        x0 = xt
+    else:
+        s = rn.rand()
+        if(np.exp(-1*(y2 - y1)/Tk) > s):
+            x0 = xt
+
+    if(y2 < lowest):
+        lowest = y2
+        print('Lowest: ', lowest)
+        lowx = x0
+
+    k = k+1
+    uk = np.append(uk, k)
+    vk = np.append(vk, obj_f(x0, w, u, y))
+    wk = np.append(wk, lowest)
+
+## Plot VFSA Results
+axs[1].plot(uk, vk, color='red')
+axs[1].plot(uk, wk, color='green')
+
+## Inefficient Divider Plotting
+wf1, wf2, wf3 = get_w(lowx)
+bf1, bf2, bf3 = get_b(lowx)
+
+print('Checking that the neural net works')
+print(f(np.array([[-1], [-1]]), wf1, wf2, wf3, bf1, bf2, bf3))
+print(f(np.array([[-1], [1]]), wf1, wf2, wf3, bf1, bf2, bf3))
+print(f(np.array([[1], [-1]]), wf1, wf2, wf3, bf1, bf2, bf3))
+print(f(np.array([[1], [1]]), wf1, wf2, wf3, bf1, bf2, bf3))
+print('Check finished\n')
+
+for i in range(10):
+    for j in range(10):
+        xcomp = -2 + (4./10)*j
+        ycomp = -1.5 + (3./10)*i
+
+        inp = np.array([[xcomp], [ycomp]])
+        l = f(inp, wf1, wf2, wf3, bf1, bf2, bf3)
+        print(l)
+        if(np.linalg.norm(f(inp, wf1, wf2, wf3, bf1, bf2, bf3)) < 1):
+            axs[0].plot(xcomp, ycomp, 'g.')
 
 
 

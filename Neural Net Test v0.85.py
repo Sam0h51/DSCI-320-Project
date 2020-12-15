459dedeb831d140f73f8d4d6c1af678776ae56ab
Sam6489 <37916482+Sam6489@users.noreply.github.com>
2020-12-15 02:08:15 -0700

    Updated v0.8, created v0.85, saved an interesting picture
    v0.8 is going to be the final version, v0.85 attempts to use batch training, but has unresolved errors.

--- Neural Net Test v0.85.py
+++ Neural Net Test v0.85.py
@@ -13,6 +13,8 @@ class Neural_Network():
         self.L3 = L3
         self.osize = osize
         self.Lrate = Lrate
+        self.counter = 0
+        self.batchsize = 5
 
         ## Initialize Weights
         self.W2 = rn.randn(L2, insize)
@@ -23,6 +25,15 @@ class Neural_Network():
         self.B2 = rn.randn(L2, 1)
         self.B3 = rn.randn(L3, 1)
         self.BL = rn.randn(osize, 1)
+
+        ## Initialize Update Storage
+        self.updateWL = np.zeros((osize, L3))
+        self.updateW3 = np.zeros((L3, L2))
+        self.updateW2 = np.zeros((L2, insize))
+
+        self.updateBL = np.zeros((osize, 1))
+        self.updateB3 = np.zeros((L3, 1))
+        self.updateB2 = np.zeros((L2, 1))
 
         print('W2: ', self.W2, '\nW3', self.W3, '\nWL', self.WL, '\nB2', self.B2, '\nB3', self.B3, '\nBL', self.BL)
 
@@ -37,10 +48,12 @@ class Neural_Network():
     def forward_prop(self, xk):
         ## Input
         self.a1 = xk
+##        print('a1:\n', self.a1)
 
         ## First Inner Layer Activation
         self.z2 = self.W2.dot(self.a1) + self.B2
         self.a2 = self.sigma(self.z2)
+##        print('a2:\n', self.a2)
 
         ## Compute the diagonal gradient matrix
         self.D2 = np.diag(self.sigma_prime(self.a2).reshape(self.L2, ))
@@ -49,11 +62,15 @@ class Neural_Network():
         self.z3 = self.W3.dot(self.a2) + self.B3
         self.a3 = self.sigma(self.z3)
         self.D3 = np.diag(self.sigma_prime(self.a3).reshape(self.L3, ))
+##        print('a3:\n', self.a3)
 
         ## Output Layer Activation
         self.zL = self.WL.dot(self.a3) + self.BL
         self.aL = self.sigma(self.zL)
         self.DL = np.diag(self.sigma_prime(self.aL).reshape(self.osize, ))
+##        print('aL:\n', self.a3)
+
+##        print(self.a1, self.z2, self.a2, self.D2, self.z3, self.a3, self.D3, self.zL, self.aL, self.DL)
 
         return self.aL
 
@@ -63,14 +80,47 @@ class Neural_Network():
         self.delta3 = self.D3.dot(self.WL.T.dot(self.deltaL))
         self.delta2 = self.D2.dot(self.W3.T.dot(self.delta3))
 
-        ## Update Weights and Biases
-        self.WL = self.WL - self.Lrate*self.deltaL.dot(self.a3.T)
-        self.W3 = self.W3 - self.Lrate*self.delta3.dot(self.a2.T)
-        self.W2 = self.W2 - self.Lrate*self.delta2.dot(self.a1.T)
+        self.counter += 1
 
-        self.BL = self.BL - self.Lrate*self.deltaL
-        self.B3 = self.B3 - self.Lrate*self.delta3
-        self.B2 = self.B2 - self.Lrate*self.delta2
+        ## Sum delta values for batch update
+        if (self.counter == 0):
+            self.updateWL = self.Lrate*self.deltaL.dot(self.a3.T)
+            self.updateW3 = self.Lrate*self.delta3.dot(self.a2.T)
+            self.updateW2 = self.Lrate*self.delta2.dot(self.a1.T)
+
+            self.updateBL = self.Lrate*self.deltaL
+            self.updateB3 = self.Lrate*self.delta3
+            self.updateB2 = self.Lrate*self.delta2
+        elif (self.counter == self.batchsize):
+##            print(self.updateWL)
+            self.WL = self.WL - self.updateWL/self.batchsize
+            self.W3 = self.W3 - self.updateW3/self.batchsize
+            self.W2 = self.W2 - self.updateW2/self.batchsize
+
+            self.BL = self.BL - self.updateBL/self.batchsize
+            self.B3 = self.B3 - self.updateBL/self.batchsize
+            self.B2 = self.B2 - self.updateBL/self.batchsize
+
+            self.counter = 0
+        else:
+            self.updateWL += self.Lrate*self.deltaL.dot(self.a3.T)
+            self.updateW3 += self.Lrate*self.delta3.dot(self.a2.T)
+            self.updateW2 += self.Lrate*self.delta2.dot(self.a1.T)
+
+            self.updateBL += self.Lrate*self.deltaL
+            self.updateB3 += self.Lrate*self.delta3
+            self.updateB2 += self.Lrate*self.delta2
+
+        ## Update Weights and Biases
+##        self.WL = self.WL - self.Lrate*self.deltaL.dot(self.a3.T)
+##        self.W3 = self.W3 - self.Lrate*self.delta3.dot(self.a2.T)
+##        self.W2 = self.W2 - self.Lrate*self.delta2.dot(self.a1.T)
+##
+##        self.BL = self.BL - self.Lrate*self.deltaL
+##        self.B3 = self.B3 - self.Lrate*self.delta3
+##        self.B2 = self.B2 - self.Lrate*self.delta2
+
+##        print(self.a1, self.z2, self.a2, self.D2, self.z3, self.a3, self.D3, self.zL, self.aL, self.DL)
 
     def train_network(self, xk, target):
         o = self.forward_prop(xk)
@@ -83,7 +133,6 @@ class Data_Ingest():
 
         self.xdim = xdim
         self.pull_data(filename, ignore_top, xdim)
-        self.normalized = self.X
 
     def pull_data(self, filename, ignore_top, xdim):
 
@@ -123,115 +172,37 @@ class Data_Ingest():
         return self.consistent, self.con_y
 
     def normalize_data(self):
-        self.normalized = self.X.T
-        a, b = self.normalized.shape
+        normalized = self.X.T
+        a, b = normalized.shape
         for i in range(a):
-            self.normalized[i] = (self.normalized[i] - np.amin(self.normalized[i]))/(np.amax(self.normalized[i]) - np.amin(self.normalized[i]))
-        self.normalized = self.normalized.T
-        return self.normalized, self.Y
+            normalized[i] = normalized[i]/np.amax(normalized[i])
+        return normalized.T
 
     def normalize_consistent_data(self):
-        normalized_con = self.consistent.T
+        normalized = self.consistent.T
         a, b = normalized.shape
         for i in range(a):
             normalized[i] = (normalized[i] - np.amin(normalized[i]))/(np.amax(normalized[i]) - np.amin(normalized[i]))
-        return normalized.T, self.con_y
-
-    def get_split_consistent(self, amount):
-        X = np.array(())
-        y = np.array(())
-        T = np.array(())
-        u = np.array(())
-
-        for i in range(self.Y.size):
-            s = rn.rand()
-            if(s > 0.2):
-                X = np.append(X, self.normalized[i])
-                y = np.append(y, self.Y[i])
-            else:
-                T = np.append(T, self.normalized[i])
-                u = np.append(u, self.Y[i])
-
-        X = X.reshape(int(X.size/self.xdim), self.xdim)
-        T = T.reshape(int(T.size/self.xdim), self.xdim)
-        a, b = X.shape
-
-        consistent = np.array(())
-        con_y = np.array(())
-
-        for i in range(amount):
-            s = math.floor(rn.uniform(0, a))
-            consistent = np.append(consistent, X[s])
-            con_y = np.append(con_y, y[s])
-
-        self.consistent = consistent.reshape(int(consistent.size/self.xdim), self.xdim)
-        self.con_y = con_y
-
-        self.split_conX = X
-        self.split_conY = y
-        self.split_conT = T
-        self.split_conU = u
-
-        return self.consistent, self.con_y, T, u
-
-    def get_split_XandY(self):
-        return self.split_conX, self.split_conY
-
-## Function to average testing set data
-def average_error(X, y):
-    error = 0
-    for i in range(y.size):
-        error += (NeuNet.forward_prop(X[i].reshape(10, 1)) - y[i])**2
-    return error/y.size
-
-def produce_confusion_matrix(X, y, NN):
-    output = np.array(())
-    for i in range(y.size):
-        output = np.append(output, NN.forward_prop(X[i].reshape(10, 1)))
-    output = np.round(output)
-
-    con_matrix = np.array(([0, 0], [0, 0]))
-
-    for i in range(y.size):
-        if (y[i] == 0):
-            if(output[i] == 0):
-                con_matrix[0][0] += 1
-            elif(output[i] == 1):
-                con_matrix[1][0] += 1
-            else:
-                print("ERROR SCREAM!")
-        elif(y[i] == 1):
-            if(output[i] == 0):
-                con_matrix[0][1] += 1
-            elif(output[i] == 1):
-                con_matrix[1][1] += 1
-            else:
-                print("ERROR SCREAM!")
-
-    return con_matrix
-
+        return normalized.T
 
 ## Create Data Structures
 NeuNet = Neural_Network(10, 15, 7, 1, 0.1)
-Dat = Data_Ingest('DesiredColumns.csv', 10, True)
-##Dat2 = Data_Ingest('TestingData.csv', 10)
+Dat = Data_Ingest('TrainingData.csv', 10, True)
+Dat2 = Data_Ingest('TestingData.csv', 10)
 
 ## Setup for Training
-E = 10000
-##Dat.generate_consistent_set(E)
-##X, y = Dat.normalize_consistent_data()
-##Xt, yt = Dat2.normalize_data()
-##Xk, yk = Dat.normalize_data()
+E = 5000
+X, y = Dat.generate_consistent_set(E)
+Xt, yt = Dat2.get_XandY()
+Xc, yc = Dat.get_XandY()
 
-Xs, ys = Dat.normalize_data()
-X, y, Xt, yt = Dat.get_split_consistent(E)
-Xk, yk = Dat.get_split_XandY()
+maxval = np.maximum(np.amax(X), np.amax(Xt))
+X = X/maxval
+Xt = Xt/maxval
 
-
-u = np.array(())
-v = np.array(())
-w = np.array(())
-k = 0
+X = Dat.normalize_consistent_data()
+Xc = Dat.normalize_data()
+Xt = Dat2.normalize_data()
 
 def test_error(X, y):
     a, b = X.shape
@@ -240,6 +211,7 @@ def test_error(X, y):
         outs = np.append(outs, NeuNet.forward_prop(X[i].reshape(10, 1)))
     error = outs - y
     classifications = np.round(outs, 0)
+##    print(classifications)
     misclassifications = classifications - y
 
     return np.linalg.norm(error), np.linalg.norm(misclassifications)**2
@@ -249,22 +221,21 @@ def print_out(X, y):
     print('Output\t\tExpected')
     for i in range(a):
         print(NeuNet.forward_prop(X[i].reshape(10, 1)), '\t', y[i])
-            
+
+
+
 ## Training the Neural Net
 for i in range(E):
     NeuNet.train_network(X[i].reshape(10, 1), y[i])
-    u = np.append(u, k)
-    v = np.append(v, (NeuNet.forward_prop(X[i].reshape(10, 1)) - y[i])**2)
-    w = np.append(w, average_error(Xt, yt))
-    k += 1
 
     if(i%100 == 0):
         print('Input:\n')
-        print(X[i])
+        print(X[i].reshape(10, 1))
         print('Output\t\tExpected Output')
         print(NeuNet.forward_prop(X[i].reshape(10, 1)), '\t', y[i])
         print(test_error(Xt, yt))
-        print(test_error(Xk, yk))
+    if(i%1000 == 0):
+        print(test_error(Xc, yc))
     
 
 print('Input:\n')
@@ -272,16 +243,14 @@ print(X[E-1])
 print('Output\t\tExpected Output')
 print(NeuNet.forward_prop(X[i].reshape(10, 1)), '\t', y[i])
 print(test_error(Xt, yt))
-print('\n\nOUTPUT=S:')
-print_out(Xk, yk)
-print('\n\nConfusion Matrix:')
-print(produce_confusion_matrix(Xk, yk, NeuNet))
-print(produce_confusion_matrix(Xt, yt, NeuNet))
-print(produce_confusion_matrix(Xs, ys, NeuNet))
+print('\n\n\nTesting Data full output:')
+print(NeuNet.WL, '\n\n', NeuNet.W3, '\n\n', NeuNet.W2, '\n\n', NeuNet.BL, NeuNet.B3, NeuNet.B2)
+print_out(Xt, yt)
 
-plt.plot(u, v, color='red')
-plt.plot(u, w, color='green')
-plt.show()
+
+##print('\n\nOUTPUT=S:')
+##print_out(X, y)
+
 
 
 
